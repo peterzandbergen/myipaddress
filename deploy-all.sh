03eda8489255271c214b4d9e47e29f812a10e679
Peter Zandbergen <peter.zandbergen@myhops.com>
2019-11-17 14:42:10 +0100

    dockerized

--- deploy-all.sh
+++ deploy-all.sh
@@ -2,4 +2,4 @@
 
 docker build --tag peterzandbergen/myipaddress:scratch . && \
 docker push peterzandbergen/myipaddress:scratch && \
-cf push --docker-image peterzandbergen/myipaddress:scratch
+cf push # --docker-image peterzandbergen/myipaddress:scratch
